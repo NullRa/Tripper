d10b9b9b563b0ba1e5875e417302667d6955d81f
jyang <jTang@walkgame.com>
2022-07-29 14:30:15 +0800

    init proj

---
+++ Tripper/Tripper/TripperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripperApp.swift
+//  Tripper
+//
+//  Created by Apple on 2022/7/29.
+//
+
+import SwiftUI
+
+@main
+struct TripperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
